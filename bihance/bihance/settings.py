7fd8bb0f1a53faebb50df7641d8534f3a8bdbe86
iamsven2005 <iamsven2005@gmail.com>
2025-04-24 17:24:12 +0800

    deploment 2

--- bihance/bihance/settings.py
+++ bihance/bihance/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-^6k46i6&49f@8hg-&juek)6-cn66mg2jqlsm9jzzc+7@hi5))k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bihance-django.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
