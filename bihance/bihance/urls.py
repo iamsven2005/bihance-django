c34841cdeb88aeae3b36099db6a0295391cc8ced
YangQF2002 <yangqf18@gmail.com>
2025-05-04 00:52:26 +0800

    Established /messages endpoint

--- bihance/bihance/urls.py
+++ bihance/bihance/urls.py
@@ -1,5 +1,6 @@
 from applications.views import ApplicationsViewSet
 from availabilities.views import AvailabilitiesViewSet
+from message.views import MessageViewSet
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers 
@@ -8,6 +9,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r"applications", ApplicationsViewSet)
 router.register(r"availabilities", AvailabilitiesViewSet)
+router.register(r"messages", MessageViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
