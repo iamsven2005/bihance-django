3a4120eef74f35a485f5241e9d5f84bad2be6889
YangQF2002 <yangqf18@gmail.com>
2025-05-21 19:19:49 +0800

    Minor fixes

--- bihance/files/models.py
+++ bihance/files/models.py
@@ -1,13 +1,59 @@
+from applications.models import User, Job
+from companies.models import EmployerProfile
 from django.db import models
 from django.utils import timezone
+from functools import reduce
 from message.models import Message
+from operator import or_
 
 
-# Predefined set of options for associated_type field
 class AssociatedType(models.TextChoices):
     MESSAGE = 'Message'
     GROUP_MESSAGE = 'Group Message'
+    USER = "User", 
+    JOB = "Job"
+    COMPANY = "Company"
 
+atype_to_condition_map = {
+    AssociatedType.MESSAGE: 'associated_message__isnull',
+    AssociatedType.GROUP_MESSAGE: 'associated_group_message__isnull',
+    AssociatedType.USER: 'associated_user__isnull',
+    AssociatedType.JOB: 'associated_job__isnull',
+    AssociatedType.COMPANY: 'associated_company__isnull',
+}
+
+
+# Each Q object represents a criteria
+# The smaller conditions, that make up the criteria
+# Is given as a collection of key-value pairs
+def check_associated_objects():
+    q_objects_list = []
+
+    for atype in AssociatedType.values: 
+        condition_dict = create_condition_dictionary(atype)
+        q_object = models.Q(**condition_dict)
+        q_objects_list.append(q_object)
+
+    # Reduce over all Q objects, using OR to join them 
+    # Obtain a single Q object (that basically says, only ONE Q object needs to be satisfied)
+    return reduce(or_, q_objects_list)
+    
+def create_condition_dictionary(atype): 
+    result = {}
+    result["associated_type"] = atype
+
+    # Identify the false isnull condition
+    # All other isnull conditions will be true 
+    false_condition = atype_to_condition_map[atype]
+    conditions = atype_to_condition_map.values()
+
+    for condition in conditions: 
+        if condition == false_condition: 
+            result[condition] = False
+        else: 
+            result[condition] = True
+
+    return result 
 
 # References: 
 # https://abenezer.org/blog/generic-foreign-key-check-constraint
@@ -22,29 +68,27 @@ class Files(models.Model):
 
     associated_type = models.TextField(choices=AssociatedType.choices, db_column="associatedType")    
     associated_message = models.ForeignKey(Message, null=True, blank=True, on_delete=models.PROTECT, db_column="associatedMessage")
+    # associated_group_message = models.ForeignKey(Groupmessage, null=True, blank=True, on_delete=models.PROTECT) [IN FUTURE]
     associated_group_message = models.TextField(null=True, blank=True, db_column="associatedGroupMessage")
-    # associated_group_message = models.ForeignKey(Groupmessage, null=True, blank=True, on_delete=models.PROTECT)
+
+    associated_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, db_column="associatedUser")
+    associated_job = models.ForeignKey(Job, null=True, blank=True, on_delete=models.PROTECT, db_column="associatedJob")
+    associated_company = models.ForeignKey(EmployerProfile, null=True, blank=True, on_delete=models.PROTECT, db_column="associatedCompany")
+    
 
     class Meta:
         db_table = 'files'
         indexes = [
             models.Index(fields=['associated_message']),
-            models.Index(fields=['associated_group_message'])
+            models.Index(fields=['associated_group_message']),
+            models.Index(fields=['associated_user']),
+            models.Index(fields=['associated_job']),
+            models.Index(fields=['associated_company'])
         ]
         constraints = [
+            # Row level check constraint
             models.CheckConstraint(
-                check=(
-                    models.Q(
-                        associated_type=AssociatedType.MESSAGE, 
-                        associated_group_message__isnull=True,
-                        associated_message__isnull=False,
-                    ) |
-                    models.Q(
-                        associated_type=AssociatedType.GROUP_MESSAGE,
-                        associated_message__isnull=True,
-                        associated_group_message__isnull=False,
-                    )
-                ),
+                check=check_associated_objects(),
                 name='%(app_label)s_%(class)s_only_one_associated_object',
             )
         ]
--- bihance/files/views.py
+++ bihance/files/views.py
@@ -1,9 +1,18 @@
 from .models import Files
 from .serializers import FilesSerializer, FilesCreateInputSerializer, FilesListInputSerializer
+from applications.models import User, Job
+from companies.models import EmployerProfile
 from django.http import JsonResponse, HttpResponse
 from message.models import Message
 from rest_framework import permissions, viewsets
 
+
+atype_to_aobject_lookup_map = {
+    "Message": (Message, "message_id"),
+    "User": (User, "id"),
+    "Job": (Job, "job_id"),
+    "Company": (EmployerProfile, "company_id"),
+}
 
 class FilesViewSet(viewsets.ModelViewSet): 
     permission_classes = [permissions.IsAuthenticated]
@@ -20,13 +29,16 @@ class FilesViewSet(viewsets.ModelViewSet):
         associated_object_id = validated_data["associatedObjectId"]
 
         # Try to retrieve the associated object 
-        match associated_type: 
-            case "Message": 
-                try: 
-                    Message.objects.get(message_id=associated_object_id)
-                except Message.DoesNotExist: 
-                    return HttpResponse("No associated object found.", status=400)
-
+        if associated_type in atype_to_aobject_lookup_map: 
+            aobject_lookup_class, aobject_lookup_field = atype_to_aobject_lookup_map[associated_type]
+            
+            try: 
+                # Directly passing the kv pair won't work 
+                # Since the lookup_field is interpreted literally, not dynamically
+                aobject_lookup_class.objects.get(**{aobject_lookup_field: associated_object_id})
+            except aobject_lookup_class.DoesNotExist: 
+                return HttpResponse("No associated object found.", status=400)
+            
         # Use the associated type to get associated object 
         # Then, get all files linked to associated object
         match associated_type: 
@@ -55,13 +67,14 @@ class FilesViewSet(viewsets.ModelViewSet):
         associated_object_id = validated_data["associatedObjectId"]
 
         # Try to retrieve the associated object 
-        match associated_type: 
-            case "Message": 
-                try: 
-                    associated_object = Message.objects.get(message_id=associated_object_id)
-                except Message.DoesNotExist: 
-                    return HttpResponse("No associated object found.", status=400)
-
+        if associated_type in atype_to_aobject_lookup_map: 
+            aobject_lookup_class, aobject_lookup_field = atype_to_aobject_lookup_map[associated_type]
+            
+            try: 
+                associated_object = aobject_lookup_class.objects.get(**{aobject_lookup_field: associated_object_id})
+            except aobject_lookup_class.DoesNotExist: 
+                return HttpResponse("No associated object found.", status=400)
+            
         # Create the new file record 
         file = Files(
             file_key=file_key, 
